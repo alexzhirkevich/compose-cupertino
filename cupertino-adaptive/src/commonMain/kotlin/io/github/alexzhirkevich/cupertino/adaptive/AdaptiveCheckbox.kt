63232f0913f765fe8812413160239d461c308981
Alexander Zhirkevich <sasha.zhirkevich@gmail.com>
2024-02-27 11:35:48 +0300

    New components: `CupertinoCheckbox`, `CupertinoTriStateCheckbox`, `AdaptiveCheckbox`, `AdaptiveTriStateCheckbox` (#29)
    ![image](https://github.com/alexzhirkevich/compose-cupertino/assets/63979218/8e21c03f-a86b-4732-a4c7-4ecc7a117bbf)
    
    ---------
    
    Co-authored-by: Zhirkevich Alexander Y <ZhirkevichAY@bsu.by>

---
+++ cupertino-adaptive/src/commonMain/kotlin/io/github/alexzhirkevich/cupertino/adaptive/AdaptiveCheckbox.kt
@@ -0,0 +1,120 @@
+package io.github.alexzhirkevich.cupertino.adaptive
+
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxColors
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.TriStateCheckbox
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.state.ToggleableState
+import io.github.alexzhirkevich.cupertino.CupertinoCheckBox
+import io.github.alexzhirkevich.cupertino.CupertinoCheckboxColors
+import io.github.alexzhirkevich.cupertino.CupertinoCheckboxDefaults
+import io.github.alexzhirkevich.cupertino.CupertinoTriStateCheckBox
+
+@ExperimentalAdaptiveApi
+@Composable
+fun AdaptiveCheckbox(
+    checked: Boolean,
+    onCheckedChange: ((Boolean) -> Unit)?,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    adaptation : AdaptationScope<CupertinoCheckBoxAdaptation,MaterialCheckBoxAdaptation>.() -> Unit = {}
+) {
+    AdaptiveWidget(
+        adaptation = remember { CheckBoxAdaptation() },
+        adaptationScope = adaptation,
+        material = {
+            Checkbox(
+                checked = checked,
+                onCheckedChange = onCheckedChange,
+                modifier = modifier,
+                enabled = enabled,
+                interactionSource = interactionSource,
+                colors = it.colors
+            )
+        },
+        cupertino = {
+            CupertinoCheckBox(
+                checked = checked,
+                onCheckedChange = onCheckedChange,
+                modifier = modifier,
+                enabled = enabled,
+                interactionSource = interactionSource,
+                colors = it.colors
+            )
+        }
+    )
+}
+
+@ExperimentalAdaptiveApi
+@Composable
+fun AdaptiveTriStateCheckbox(
+    state: ToggleableState,
+    onClick: (() -> Unit)?,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    adaptation : AdaptationScope<CupertinoCheckBoxAdaptation,MaterialCheckBoxAdaptation>.() -> Unit = {}
+) {
+    AdaptiveWidget(
+        adaptation = remember { CheckBoxAdaptation() },
+        adaptationScope = adaptation,
+        material = {
+            TriStateCheckbox(
+                state = state,
+                onClick = onClick,
+                modifier = modifier,
+                enabled = enabled,
+                interactionSource = interactionSource,
+                colors = it.colors
+            )
+        },
+        cupertino = {
+            CupertinoTriStateCheckBox(
+                state = state,
+                onClick = onClick,
+                modifier = modifier,
+                enabled = enabled,
+                interactionSource = interactionSource,
+                colors = it.colors
+            )
+        }
+    )
+}
+
+class MaterialCheckBoxAdaptation(
+    var colors: CheckboxColors
+)
+
+class CupertinoCheckBoxAdaptation(
+    var colors : CupertinoCheckboxColors
+)
+
+@OptIn(ExperimentalAdaptiveApi::class)
+@Stable
+private class CheckBoxAdaptation : Adaptation<CupertinoCheckBoxAdaptation,MaterialCheckBoxAdaptation>(){
+
+    @Composable
+    override fun rememberCupertinoAdaptation(): CupertinoCheckBoxAdaptation {
+        val colors = CupertinoCheckboxDefaults.colors()
+
+        return remember(colors) {
+            CupertinoCheckBoxAdaptation(colors)
+        }
+    }
+
+    @Composable
+    override fun rememberMaterialAdaptation(): MaterialCheckBoxAdaptation {
+        val colors = CheckboxDefaults.colors()
+
+        return remember(colors) {
+            MaterialCheckBoxAdaptation(colors)
+        }
+    }
+
+}
