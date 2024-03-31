242a672183a0e0cde5096fa88c579a406e3ea236
Александр Жиркевич <sasha.zhirkevich@gmail.com>
2024-03-31 14:15:57 +0300

    vertical divider

--- cupertino-adaptive/src/commonMain/kotlin/io/github/alexzhirkevich/cupertino/adaptive/AdaptiveDivider.kt
+++ cupertino-adaptive/src/commonMain/kotlin/io/github/alexzhirkevich/cupertino/adaptive/AdaptiveDivider.kt
@@ -19,6 +19,7 @@ package io.github.alexzhirkevich.cupertino.adaptive
 
 import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
@@ -30,6 +31,22 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import io.github.alexzhirkevich.cupertino.CupertinoDivider
 import io.github.alexzhirkevich.cupertino.CupertinoDividerDefaults
+import io.github.alexzhirkevich.cupertino.CupertinoHorizontalDivider
+import io.github.alexzhirkevich.cupertino.CupertinoVerticalDivider
+
+@Deprecated(
+    replaceWith = ReplaceWith(
+        "AdaptiveHorizontalDivider(modifier,adaptation)",
+        "io.github.alexzhirkevich.cupertino.adaptive.AdaptiveHorizontalDivider"
+    ),
+    message = "Use AdaptiveHorizontalDivider instead")
+@Composable
+@ExperimentalAdaptiveApi
+fun AdaptiveDivider(
+    modifier: Modifier = Modifier,
+    adaptation: AdaptationScope<DividerAdaptation,DividerAdaptation>.() -> Unit = {}
+) = AdaptiveHorizontalDivider(modifier, adaptation)
+
 
 @Composable
 @ExperimentalAdaptiveApi
@@ -50,7 +67,35 @@ fun AdaptiveHorizontalDivider(
             )
         },
         cupertino = {
-            CupertinoDivider(
+            CupertinoHorizontalDivider(
+                modifier = modifier,
+                thickness = it.thickness,
+                color = it.color
+            )
+        }
+    )
+}
+
+@Composable
+@ExperimentalAdaptiveApi
+fun AdaptiveVerticalDivider(
+    modifier: Modifier = Modifier,
+    adaptation: AdaptationScope<DividerAdaptation,DividerAdaptation>.() -> Unit = {}
+) {
+    AdaptiveWidget(
+        adaptation = remember {
+            DividerAdaptationScope()
+        },
+        adaptationScope = adaptation,
+        material = {
+            VerticalDivider(
+                modifier = modifier,
+                thickness = it.thickness,
+                color = it.color
+            )
+        },
+        cupertino = {
+            CupertinoVerticalDivider(
                 modifier = modifier,
                 thickness = it.thickness,
                 color = it.color
