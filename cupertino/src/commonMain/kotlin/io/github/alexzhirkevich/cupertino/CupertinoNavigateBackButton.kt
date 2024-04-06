b6a02bb89ddf06b97b606a4c4d13840dc6aa8739
Александр Жиркевич <sasha.zhirkevich@gmail.com>
2024-04-06 16:37:23 +0300

    fix navigate back button arrangement

--- cupertino/src/commonMain/kotlin/io/github/alexzhirkevich/cupertino/CupertinoNavigateBackButton.kt
+++ cupertino/src/commonMain/kotlin/io/github/alexzhirkevich/cupertino/CupertinoNavigateBackButton.kt
@@ -20,11 +20,13 @@ package io.github.alexzhirkevich.cupertino
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -64,14 +66,18 @@ fun CupertinoNavigateBackButton(
         interactionSource = interactionSource,
         colors = colors
     ) {
-        CupertinoIcon(
-            imageVector = icon,
-            contentDescription = null,
-            modifier = Modifier
-                .height(CupertinoIconDefaults.MediumSize)
-                .padding(end = 6.dp)
-        )
-        title()
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            CupertinoIcon(
+                imageVector = icon,
+                contentDescription = null,
+                modifier = Modifier
+                    .height(CupertinoIconDefaults.MediumSize)
+                    .padding(end = 6.dp)
+            )
+            title()
+        }
     }
 }
 
